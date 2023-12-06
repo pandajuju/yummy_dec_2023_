5519fa24a0e713f0ab0ccaa0326a86f7ef480c1f
Ustimova <u.juliana.serg@gmail.com>
2023-12-06 10:18:16 +0200

    Added Dish, static

--- yummy/admin.py
+++ yummy/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import DishCategory
+from .models import DishCategory, Dish
 from django.utils.safestring import mark_safe
 
 admin.site.register(DishCategory)
 
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+    list_display = ('id', 'category', 'name', 'price', 'is_visible', 'photo_src_tag')
+    list_editable = ('category', 'name', 'price', 'is_visible')
+    search_fields = ('name',)
+    list_filter = ('category', 'price', 'is_visible')
+
+    def photo_src_tag(self, obj):
+        if obj.photo:
+            return mark_safe(f"<img src='{obj.photo.url}' width=50>")
+
+    photo_src_tag.short_description = 'Dish photo'
